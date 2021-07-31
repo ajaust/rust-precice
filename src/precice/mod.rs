937ca2ca028ae4aaef23b254cff282d9c1634220
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2021-07-31 18:31:19 +0200

    [wip] add more interface functions

--- src/precice/mod.rs
+++ src/precice/mod.rs
@@ -4,7 +4,7 @@ mod precice_bindings;
 //pub mod precice;
 
 //use precice::libc::c_char;
-use precice::libc::{c_int, c_char};
+use precice::libc::{c_int};
 use std::ffi::CString;
 use std::ffi::CStr;
 
@@ -56,32 +56,32 @@ pub fn is_coupling_ongoing() -> bool
 
 pub fn is_read_data_available() -> bool
 {
-    let coupling_is_ongoing: i32 = unsafe {  precice_bindings::precicec_isReadDataAvailable() };
-    coupling_is_ongoing != 0
+    let read_data_is_available: i32 = unsafe {  precice_bindings::precicec_isReadDataAvailable() };
+    read_data_is_available != 0
 }
 
 pub fn is_write_data_required( computed_time_step_length: f64 ) -> bool
 {
-    let coupling_is_ongoing: i32 = unsafe {  precice_bindings::precicec_isWriteDataRequired( computed_time_step_length ) };
-    coupling_is_ongoing != 0
+    let write_data_is_available: i32 = unsafe {  precice_bindings::precicec_isWriteDataRequired( computed_time_step_length ) };
+    write_data_is_available != 0
 }
 
 pub fn is_time_window_complete() -> bool
 {
-    let coupling_is_ongoing: i32 = unsafe {  precice_bindings::precicec_isTimeWindowComplete() };
-    coupling_is_ongoing != 0
+    let time_window_is_complete: i32 = unsafe {  precice_bindings::precicec_isTimeWindowComplete() };
+    time_window_is_complete != 0
 }
 
 pub fn has_to_evaluate_surrogate_model() -> bool
 {
-    let coupling_is_ongoing: i32 = unsafe {  precice_bindings::precicec_hasToEvaluateSurrogateModel() };
-    coupling_is_ongoing != 0
+    let surrogate_model_has_to_be_evaluated: i32 = unsafe {  precice_bindings::precicec_hasToEvaluateSurrogateModel() };
+    surrogate_model_has_to_be_evaluated != 0
 }
 
 pub fn has_to_evaluate_fine_model() -> bool
 {
-    let coupling_is_ongoing: i32 = unsafe {  precice_bindings::precicec_hasToEvaluateFineModel() };
-    coupling_is_ongoing != 0
+    let fine_model_has_to_be_evaluated: i32 = unsafe {  precice_bindings::precicec_hasToEvaluateFineModel() };
+    fine_model_has_to_be_evaluated != 0
 }
 
 pub enum PreciceActions {
@@ -102,11 +102,99 @@ impl PreciceActions {
 
 pub fn is_action_required( action: PreciceActions ) -> bool
 {
-    let coupling_is_ongoing: i32 = unsafe {  precice_bindings::precicec_isActionRequired( action.as_cstring().as_ptr() ) };
-    coupling_is_ongoing != 0
+    let action_is_required: i32 = unsafe {  precice_bindings::precicec_isActionRequired( action.as_cstring().as_ptr() ) };
+    action_is_required != 0
 }
 
-pub fn mark_action_fulfilled( action: PreciceActions ) -> bool
+pub fn mark_action_fulfilled( action: PreciceActions )
 {
-    precicec_markActionFulfilled;
+    unsafe{ precice_bindings::precicec_markActionFulfilled( action.as_cstring().as_ptr() ) };
+}
+
+pub fn has_mesh( mesh_name: String ) -> bool
+{
+    let mesh_name = CString::new(mesh_name).unwrap();
+    let has_mesh_with_name: i32 = unsafe{ precice_bindings::precicec_hasMesh( mesh_name.as_ptr() ) };
+    has_mesh_with_name != 0
+}
+
+pub fn get_mesh_id( mesh_name: String) -> i32
+{
+    let mesh_name = CString::new(mesh_name).unwrap();
+    let mesh_id = unsafe{ precice_bindings::precicec_getMeshID( mesh_name.as_ptr() ) };
+    mesh_id
+}
+
+pub fn set_mesh_vertex( mesh_id: i32, position: &[f64]) -> i32
+{
+    let vertex_id = unsafe{ precice_bindings::precicec_setMeshVertex( mesh_id as c_int, position.as_ptr() ) };
+    vertex_id
+}
+
+pub fn get_mesh_vertex_size( mesh_id: i32 ) -> i32
+{
+    let number_of_mesh_vertexes = unsafe{ precice_bindings::precicec_getMeshVertexSize( mesh_id as c_int ) };
+    number_of_mesh_vertexes
+}
+
+pub fn set_mesh_vertices( mesh_id: i32, positions: &[f64], ids: &mut [i32] )
+{
+    unsafe { precice_bindings::precicec_setMeshVertices( mesh_id as c_int, ids.len() as c_int, positions.as_ptr(), ids.as_mut_ptr() ) };
+}
+
+pub fn get_mesh_vertices( mesh_id: i32, ids: &[i32], positions: &mut [f64] )
+{
+    unsafe { precice_bindings::precicec_getMeshVertices( mesh_id as c_int, ids.len() as c_int, ids.as_ptr(), positions.as_mut_ptr(), ) };
+}
+
+
+pub fn get_mesh_vertex_ids_from_positions( mesh_id: i32, positions: &[f64], ids: &mut [i32] )
+{
+    unsafe { precice_bindings::precicec_getMeshVertexIDsFromPositions( mesh_id as c_int, ids.len() as c_int, positions.as_ptr(), ids.as_mut_ptr() ) };
+}
+
+pub fn set_mesh_edge( mesh_id: i32, first_vertex_id: i32, second_vertex_id: i32) -> i32
+{
+    let edge_id: i32 = unsafe { precice_bindings::precicec_setMeshEdge( mesh_id as c_int, first_vertex_id as c_int, second_vertex_id as c_int ) };
+    edge_id
+}
+
+pub fn set_mesh_triangle( mesh_id: i32, first_edge_id: i32, second_edge_id: i32, third_edge_id: i32 )
+{
+    unsafe { precice_bindings::precicec_setMeshTriangle( mesh_id as c_int, first_edge_id as c_int, second_edge_id as c_int, third_edge_id as c_int ) };
+}
+
+
+pub fn set_mesh_triangle_with_edges( mesh_id: i32, first_vertex_id: i32, second_vertex_id: i32, third_vertex_id: i32 )
+{
+    unsafe { precice_bindings::precicec_setMeshTriangleWithEdges( mesh_id as c_int, first_vertex_id as c_int, second_vertex_id as c_int, third_vertex_id as c_int ) };
+}
+
+pub fn set_mesh_quad( mesh_id: i32, first_edge_id: i32, second_edge_id: i32, third_edge_id: i32, fourth_edge_id: i32 )
+{
+    unsafe { precice_bindings::precicec_setMeshQuad( mesh_id as c_int, first_edge_id as c_int, second_edge_id as c_int, third_edge_id as c_int, fourth_edge_id as c_int  ) };
+}
+
+pub fn set_mesh_quad_with_edges( mesh_id: i32, first_vertex_id: i32, second_vertex_id: i32, third_vertex_id: i32, fourth_vertex_id: i32 )
+{
+    unsafe { precice_bindings::precicec_setMeshQuadWithEdges( mesh_id as c_int, first_vertex_id as c_int, second_vertex_id as c_int, third_vertex_id as c_int, fourth_vertex_id as c_int ) };
+}
+
+pub fn has_data( data_name: String, mesh_id: i32) -> bool
+{
+    let data_name = CString::new(data_name).unwrap();
+    let has_data_with_name: i32 = unsafe {  precice_bindings::precicec_hasData( data_name.as_ptr(), mesh_id as c_int ) };
+    has_data_with_name != 0
+}
+
+pub fn get_data_id( data_name: String, mesh_id: i32) -> i32
+{
+    let data_name = CString::new(data_name).unwrap();
+    let data_id: i32 = unsafe {  precice_bindings::precicec_getDataID( data_name.as_ptr(), mesh_id as c_int ) };
+    data_id
+}
+
+pub fn map_read_data_to( to_mesh_id: i32)
+{
+    unsafe {  precice_bindings::precicec_mapReadDataTo( to_mesh_id as c_int ) };
 }
